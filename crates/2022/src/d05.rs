6459fc09ac8656948e6c3067ed2fdc0e7f7a3235
Clemens Wasser <clemens.wasser@gmail.com>
2022-12-09 19:36:28 +0100

    Improve y2022 d05 impl

--- crates/2022/src/d05.rs
+++ crates/2022/src/d05.rs
@@ -15,31 +15,33 @@ fn parse_crates(crates: &[&str]) -> Vec<Vec<char>> {
     stacks
 }
 
-fn simulate_crane_ordered(stacks: &mut Vec<Vec<char>>, moves: &[&str]) {
+fn simulate_crane(
+    stacks: &mut Vec<Vec<char>>,
+    moves: &[&str],
+    transfer: fn(stacks: &mut Vec<Vec<char>>, from: usize, to: usize, count: usize),
+) {
     for line in moves {
         let instrs: Vec<_> = line.split_ascii_whitespace().collect();
         let count: usize = instrs[1].parse().unwrap();
         let from = instrs[3].parse::<usize>().unwrap() - 1;
         let to = instrs[5].parse::<usize>().unwrap() - 1;
-        let count_idx = stacks[from].len() - count;
-        let (_, crates) = stacks[from].split_at(count_idx);
-        let crates = crates.to_owned();
-        stacks[to].extend(crates);
-        stacks[from].drain(count_idx..);
+        transfer(stacks, from, to, count);
     }
 }
 
-fn simulate_crane(stacks: &mut Vec<Vec<char>>, moves: &[&str]) {
-    for line in moves {
-        let instrs: Vec<_> = line.split_ascii_whitespace().collect();
-        let count: usize = instrs[1].parse().unwrap();
-        let from = instrs[3].parse::<usize>().unwrap() - 1;
-        let to = instrs[5].parse::<usize>().unwrap() - 1;
-        for _ in 0..count {
-            let val = stacks[from].pop().unwrap();
-            stacks[to].push(val);
-        }
+fn transfer_crates(stacks: &mut Vec<Vec<char>>, from: usize, to: usize, count: usize) {
+    for _ in 0..count {
+        let val = stacks[from].pop().unwrap();
+        stacks[to].push(val);
     }
+}
+
+fn transfer_crates_ordered(stacks: &mut Vec<Vec<char>>, from: usize, to: usize, count: usize) {
+    let count_idx = stacks[from].len() - count;
+    let (_, crates) = stacks[from].split_at(count_idx);
+    let crates = crates.to_owned();
+    stacks[to].extend(crates);
+    stacks[from].drain(count_idx..);
 }
 
 pub fn d05(input: &str) -> (String, String) {
@@ -49,13 +51,13 @@ pub fn d05(input: &str) -> (String, String) {
     let moves = &moves[1..];
     let mut stacks = parse_crates(crates);
     let mut unordered_stacks = stacks.clone();
-    simulate_crane(&mut unordered_stacks, moves);
+    simulate_crane(&mut unordered_stacks, moves, transfer_crates);
     let part1: String = unordered_stacks
         .into_iter()
         .flat_map(|stack| stack.last().copied())
         .collect();
 
-    simulate_crane_ordered(&mut stacks, moves);
+    simulate_crane(&mut stacks, moves, transfer_crates_ordered);
     let part2: String = stacks
         .into_iter()
         .flat_map(|stack| stack.last().copied())
